1d6ccc5656077c81f98cdbef5b9f5af2c8aed768
Darius <darius.vilcinskis@live.com>
2016-01-27 16:17:31 +0200

    Updated unity project to use UI buttons for interstitials

---
+++ UnityIntegration/Unity Project/Assets/LoadInterstitialScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadInterstitialScript : MonoBehaviour
+{
+    public void OnClick()
+    {
+        Interop.LoadInterstitialAd();
+    }
+
+}
---
+++ UnityIntegration/Unity Project/Assets/ShowInterstitialScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowInterstitialScript : MonoBehaviour
+{
+
+    public void OnClick()
+    {
+        Interop.ShowInterstitialAd();
+    }
+}
